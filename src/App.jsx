e94d011a468615ba47fc35fe1a6bc8a25545ec2d
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-09-16 02:11:41 +0100

    Asset

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function PhoneCoverPage() {
+  return (
+      <div className="spline-container">
+        <Spline scene="https://prod.spline.design/qYdlWFWwqilLdKGi/scene.splinecode" />
+      </div>
+  );
+}
